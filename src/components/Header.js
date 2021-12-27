723289bf9aac57c274b1770347466f9cb78606cd
SaeedZahran <saeedzahran@hotmail.com>
2021-12-27 18:06:26 +0100

    (mt-2) margin top of two and this container should be a flex container (items-center) center the image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
         <header>
           {/* Top nav */}
           <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
-              <div> 
+              <div className="mt-2 flex items-center flex-grow"> 
                   <Image
                   src='https://links.papareact.com/f90'
                   width={150}
