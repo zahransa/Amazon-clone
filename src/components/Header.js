81ca486d1f806e752b3329acb288b17c00e642e0
SaeedZahran <saeedzahran@hotmail.com>
2021-12-27 18:01:22 +0100

    padding of 1 (p=1) not touching too much top container should grow  (flex-grow)
    little bit mor padding on the y-axis (py-2)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     return (
         <header>
           {/* Top nav */}
-          <div className="flex items-center bg-amazon_blue">
+          <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
               <div> 
                   <Image
                   src='https://links.papareact.com/f90'
