937efe123e9438336b0d97746dd758f23b4f62e6
SaeedZahran <saeedzahran@hotmail.com>
2021-12-28 16:06:23 +0100

    search

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
         <header>
           {/* Top nav */}
           <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
-              <div className="mt-2 flex items-center flex-grow"> 
+              <div className="mt-2 flex items-center flex-grow sm:flex-grow-0"> 
                   <Image
                   src='https://links.papareact.com/f90'
                   width={150}
@@ -13,6 +13,14 @@ function Header() {
                   
                   />
               </div>
+              {/* Search */}
+              <div>
+                  <input type="text" />
+
+
+              </div>
+
+
 
           </div>
 
