a298e81042d658c0665e1f65fbb91620a9e36dad
SaeedZahran <saeedzahran@hotmail.com>
2021-12-29 14:01:34 +0100

    circle in the basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,6 +43,8 @@ function Header() {
                   </div>
 
                   <div className="relative link flex items-center">
+
+                      <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
                      <ShoppingCartIcon className="h-10 " />
                      <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
                   </div>
