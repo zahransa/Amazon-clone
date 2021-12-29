d4113678de880756d61ba7802fa98f517293bbf7
SaeedZahran <saeedzahran@hotmail.com>
2021-12-29 18:07:56 +0100

    spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -60,11 +60,19 @@ function Header() {
 
 
 
-          <div>
-              <p>
+          <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+              <p className="link flex items-center">
                 <MenuIcon className="h-6 mr-1" />
-                All </p>
+                All 
+             </p>
+
+             <p className="link">Prime Video</p>
+
+
+
           </div>
+
+
         </header>
     );
 }
