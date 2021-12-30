65b122e286c0687b602973e32a02aecf7934b419
SaeedZahran <saeedzahran@hotmail.com>
2021-12-30 15:53:48 +0100

    transition that goes down from solid color to white first change the background to the grey color of amazon

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,35 @@
-import {carousel} from "react-responsive-carousel";
+import {Carousel} from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Banner() {
-    return (
-        <div className='relative'>
+  return (
+    <div className='relative'>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+
             
-            
+        >
+
+        
+
+        <div>
+         <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
         </div>
+                
+
+        <div>
+         <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+        </div>
+      </Carousel>  
+    </div>
     )
 }
 
-export default Banner;
+export default Banner
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100" >
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -11,9 +12,7 @@ export default function Home() {
       <Header />
 
       <main className='max-w-screen-2xl mx-auto'>
-
-        {/* Banner */}
-        <Banner/>
+        <Banner />
 
         {/* ProductFeed */}
       </main>
