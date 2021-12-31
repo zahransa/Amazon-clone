aade623edfa14dda70c1980be714f5de87554974
SaeedZahran <saeedzahran@hotmail.com>
2021-12-31 14:36:00 +0100

    title

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,6 +6,7 @@ function Product({ id, title, price, description, category, image }) {
             <p>{category}</p>
 
             <Image src={image} height={200} width={200} objectFit="contain"/>
+            <h4>{title}</h4>
             
         </div>
     )
