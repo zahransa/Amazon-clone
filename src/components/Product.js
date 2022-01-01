5e0c04d2b38c99d26f22f53347663e1f85748c54
SaeedZahran <saeedzahran@hotmail.com>
2022-01-01 17:59:32 +0100

    import Currency

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter"
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
