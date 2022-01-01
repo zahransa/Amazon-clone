d48a73265036482c4ebd04f811f722fb5f15967f
SaeedZahran <saeedzahran@hotmail.com>
2022-01-01 22:16:00 +0100

    arrange

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,35 +17,40 @@ function Product({ id, title, price, description, category, image }) {
 
 
   return (
-    <div>
-      <p>{category}</p>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+     </p>
       <Image src={image} height={200} width={200} objectFit="contain"/>
 
-      <h4>{title}</h4>
+      <h4 className="my-3">{title}</h4>
 
       <div className="flex">
         {Array(rating)
          .fill()
          .map((_, i) => (
-           <StarIcon className="h-5" />
+           <StarIcon className="h-5 text-yellow-500" />
          ))}
       </div>  
 
-      <p>{description}</p>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
 
-      <div>
+      <div className="mb-5">
         <Currency quantity={price} currency='GBP'/>
 
 
       </div>
 
       {hasPrime && (
-          <div>
-              <img src="https://links.papareact.com/fdw" alt="" />
-          </div>
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <p className="text-xs text-gray-100">FREE Next-day Delivery</p>
+        </div>
 
 
       )}
+
+      <button className="mt-auto button">Add to Basket</button>
 
     
 
