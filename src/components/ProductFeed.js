5305d47276ea2079243b65449e7ba638b6d275f3
SaeedZahran <saeedzahran@hotmail.com>
2022-01-02 09:53:33 +0100

    merge into the banner

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -2,7 +2,7 @@ import Product from "./Product";
 
 function ProductFeed({ products }) {
     return (
-        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52">
             
             {products.map(({id,title,price,description,category, image}) => (
                 <Product
