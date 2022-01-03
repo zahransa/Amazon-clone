1a94c7ca0c702e4e83f47b80cafbee5d819a17fd
SaeedZahran <saeedzahran@hotmail.com>
2022-01-03 23:28:05 +0100

    install: yarn add next-auth yarn add firebase

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+import Providers from "next-auth/providers";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnv2kWI7c9NSX4hsojgCvkj51BXTF0kYs",
+    authDomain: "yt-sz-6ffa0.firebaseapp.com",
+    projectId: "yt-sz-6ffa0",
+    storageBucket: "yt-sz-6ffa0.appspot.com",
+    messagingSenderId: "275879706158",
+    appId: "1:275879706158:web:bd3c1cca5e3ef2ac73e9bf"
+  };
