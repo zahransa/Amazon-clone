0abc8f218d41f93236cd530afff2a97bf8b2e525
SaeedZahran <saeedzahran@hotmail.com>
2022-01-06 05:08:11 +0100

    when I click basket it takes me to checkout

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,7 +50,7 @@ const router = useRouter();
                      <p className="font-extrabold md:text-sm">& orders</p>
                   </div>
 
-                  <div className="relative link flex items-center">
+                  <div onClick={() => router.push('/checkout')} className="relative link flex items-center">
 
                       <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
                      <ShoppingCartIcon className="h-10 " />
