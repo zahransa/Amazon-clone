db2b4ec8f04a8ac890140b65b94f047ef4cab2c7
SaeedZahran <saeedzahran@hotmail.com>
2022-01-06 04:42:06 +0100

    check out

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,17 @@
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import '../styles/globals.css'
-import { Provider as AuthProvider } from "next-auth/react"
+
+
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <AuthProvider session={pageProps.session}>
       <Provider store={store}>
-       <Component {...pageProps} />
+        <Component {...pageProps} />
       </Provider>
-    </AuthProvider>
+    
    
+    
   )
 }
 
