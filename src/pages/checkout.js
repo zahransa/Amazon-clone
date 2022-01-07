2986bdbf68f73d25902871a9099e8dfeb8bd3ba5
SaeedZahran <saeedzahran@hotmail.com>
2022-01-08 00:27:18 +0100

    shopping basket

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -7,13 +7,19 @@ function Checkout() {
             
      <main className='lg:flex max-w-screen-2xl mx-auto'>
        {/* Left section*/}
-       <div>
+       <div className="flex-grow m-5 shadow-sm">
          <Image
            src="https://links.papareact.com/ikj"
            width={1020}
            height={250}
+           objectFit="contain"
+
            
          />
+
+         <div className="flex flex-col p-5 space-y-10 bg-white">
+           <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+         </div>
 
         </div>
 
