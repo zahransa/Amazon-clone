bff79575af702460e13a763b8c72e132d70dbfc3
SaeedZahran <saeedzahran@hotmail.com>
2022-01-08 16:46:54 +0100

    Selectors

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,40 @@
 import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
-import Currency from "react-currency-formatter"
+import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 
 function Product({ id, title, price, description, category, image }) {
+  const dispatch = useDispatch();
+
   const [rating] = useState(
 
     Math.floor(Math.random()*(MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
   const [hasPrime] = useState(Math.random() < 0.5)
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+
+    };
+
+
+    //sending the product as an action to the REDUX store... the basket slice
+    dispatch(addToBasket(product))
+
+
+  }; 
 
 
   return (
@@ -50,7 +71,7 @@ function Product({ id, title, price, description, category, image }) {
 
       )}
 
-      <button className="mt-auto button">Add to Basket</button>
+      <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
 
     
 
